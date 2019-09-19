6968592bfe030bc925d11cd141dd37db866ecd67
Derek Smart <derek.smart@delphix.com>
2019-09-19 11:33:14 -0400

    add path to osx script
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,5 +9,7 @@ fi
 
 #Setup OSX
 if [ $os = "macOS-10.14" ]; then
+  export PATH=${PWD}/graalvm-ce-19.0.0/Contents/Home/bin:$PATH
+  export JAVA_HOME=${PWD}/graalvm-ce-19.0.0/Contents/Home/jre
   ${PWD}/scripts/build-osx.sh
 fi
