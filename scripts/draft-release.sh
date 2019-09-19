a24aa1cd206f8ef1535a837c5610ff09a8ca0f41
Derek Smart <derek.smart@delphix.com>
2019-09-19 15:15:29 -0400

    test draft release
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

---
+++ scripts/draft-release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
+body='{
+  "tag_name": "'${1}'",
+  "target_commitish": "master",
+  "name": "'${1}'",
+  "body": "Draft release for '${1}'",
+  "draft": true,
+  "prerelease": false
+}'
+echo $body
+
+#Draft Release
+RELEASEID=curl \
+  -sSL \
+  -X POST \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+  --data "$($body)" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases}"| \
+  python -c "import sys, json; print json.load(sys.stdin)['id']"
+
+
+#Upload Assets
