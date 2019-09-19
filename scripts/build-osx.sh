f99c7e71efc298845a2aa069f5a8c21ff5f07b30
Derek Smart <derek.smart@delphix.com>
2019-09-18 21:47:39 -0400

    native-image test
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-set -ex
+which native-image
 
-version=`cat VERSION`
+#set -ex
 
-./mvnw jar:jar install:install -DgroupId=org.bouncycastle -DartifactId=bcprov-jdk15on -Dversion=1.62
+#version=`cat VERSION`
 
-native-image -cp ${PWD}/target/titan-$version-jar-with-dependencies.jar\
-    -H:Name=titan\
-    -H:Class=io.titandata.titan.Cli\
-    -H:+ReportUnsupportedElementsAtRuntime\
-    -H:ReflectionConfigurationFiles=${PWD}/config/reflect-config.json\
-    -H:ResourceConfigurationFiles=${PWD}/config/resource-config.json\
-    -H:+AddAllCharsets\
-    --initialize-at-run-time=org.bouncycastle.crypto.prng.SP800SecureRandom\
-    --initialize-at-run-time=org.bouncycastle.jcajce.provider.drbg.DRBG$Default\
-    --initialize-at-run-time=org.bouncycastle.jcajce.provider.drbg.DRBG$NonceAndIV\
-    -J-Djava.security.properties=${PWD}/java.security.overrides\
-    --allow-incomplete-classpath\
-    --enable-http\
-    --enable-https
+#./mvnw jar:jar install:install -DgroupId=org.bouncycastle -DartifactId=bcprov-jdk15on -Dversion=1.62
 
-mkdir -p ${PWD}/releases/${version}
-zip ${PWD}/releases/${version}/titan-cli-$version-darwin_amd64.zip titan
+#native-image -cp ${PWD}/target/titan-$version-jar-with-dependencies.jar\
+#    -H:Name=titan\
+#    -H:Class=io.titandata.titan.Cli\
+#    -H:+ReportUnsupportedElementsAtRuntime\
+#    -H:ReflectionConfigurationFiles=${PWD}/config/reflect-config.json\
+#    -H:ResourceConfigurationFiles=${PWD}/config/resource-config.json\
+#    -H:+AddAllCharsets\
+#    --initialize-at-run-time=org.bouncycastle.crypto.prng.SP800SecureRandom\
+#    --initialize-at-run-time=org.bouncycastle.jcajce.provider.drbg.DRBG$Default\
+#    --initialize-at-run-time=org.bouncycastle.jcajce.provider.drbg.DRBG$NonceAndIV\
+#    -J-Djava.security.properties=${PWD}/java.security.overrides\
+#    --allow-incomplete-classpath\
+#    --enable-http\
+#    --enable-https
+
+#mkdir -p ${PWD}/releases/${version}
+#zip ${PWD}/releases/${version}/titan-cli-$version-darwin_amd64.zip titan
