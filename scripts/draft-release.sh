c0cee49746226416e015e2ed4f4bae5f45cac904
Derek Smart <derek.smart@delphix.com>
2019-09-19 15:34:30 -0400

    again
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- scripts/draft-release.sh
+++ scripts/draft-release.sh
@@ -20,7 +20,7 @@ echo $body
 
 #Draft Release
 RELEASEID=$(curl -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
-  --data "$($body)" "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases}" | \
+  --data "$($body)" "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases" | \
   python -c "import sys, json; print json.load(sys.stdin)['id']")
 
 #Upload Assets
