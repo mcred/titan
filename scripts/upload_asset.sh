a8c0332b61c64b401ce836521794e2ca11b61859
Derek Smart <derek.smart@delphix.com>
2019-09-23 09:41:37 -0400

    add content length to upload
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- scripts/upload_asset.sh
+++ scripts/upload_asset.sh
@@ -5,9 +5,12 @@ set -o pipefail
 
 UPLOAD_URL=$1
 FILE=$2
+SIZE=$(wc -c $FILE | awk '{print $1}')
+
 
 #Upload Asset
 curl \
   -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Content-Length: $SIZE"\
   -H "Content-Type: application/zip" \
   --data-binary @$FILE "$UPLOAD_URL"
